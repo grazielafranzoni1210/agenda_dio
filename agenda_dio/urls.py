bb3bc45a2f3565b94f72c7e99e183e57e825718c
grazielafranzoni1210 <graziela.franzoni@gmail.com>
2021-11-10 20:49:52 -0300

    Autenticação, login, logou e decoradores

--- agenda_dio/urls.py
+++ agenda_dio/urls.py
@@ -6,5 +6,8 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
-    path('', views.index) # para redirecionar a raiz do projeto para a agenda
+    path('', views.index), # para redirecionar a raiz do projeto para a agenda
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
 ]
